f8542d7fe196b8a6f2c0a02339f787c63b0d221e
Jacob McEwan S3841062 <s3841062@student.rmit.edu.au>
2022-09-02 14:50:11 +1000

    build: :bento: Update build info and icons

--- forge.config.cjs
+++ forge.config.cjs
@@ -1,9 +1,14 @@
 const path = require('node:path');
 const { resourceLimits } = require('node:worker_threads');
 
+/**
+ * @type {import('electron-forge').}
+ */
 module.exports = {
   packagerConfig: {
     dir: "dist",
+    icon: "src/assets/icon.ico",
+    name: "GalacticComms Voice Client",
     ignore: (filePath) => {
       const include = ["", "/", "/dist", "/package.json", "/package-lock.json"]
       const result = !(include.includes(filePath) || filePath.startsWith('/dist'));
