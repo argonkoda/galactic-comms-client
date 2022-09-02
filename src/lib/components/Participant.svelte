bc277497824da582730a8d82d358289162b70842
Jacob McEwan S3841062 <s3841062@student.rmit.edu.au>
2022-09-02 16:47:07 +1000

    feat: :fire: removed participant volume disable

--- src/lib/components/Participant.svelte
+++ src/lib/components/Participant.svelte
@@ -23,7 +23,7 @@ import PopoutButton from "./PopoutButton.svelte";
   <FFTGraph fft={$fft} />
   <PopoutButton bind:popout>
     <button class="borderless" slot="activator" let:open on:click|stopPropagation={open}><span class="material-symbols-outlined">more_vert</span></button>
-    <input type="range" min=0 max=5 step=0.01 bind:value={$volume} disabled={$muted}>
+    <input type="range" min=0 max=5 step=0.01 bind:value={$volume}>
     <button class="borderless" on:click={() => muted.update(muted => !muted)}><span class="material-symbols-outlined">{$muted ? "volume_off" : "volume_up"}</span></button>
   </PopoutButton>
 </div>
