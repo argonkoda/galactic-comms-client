6c98f617723d64675c0dd27fc6a65099d674f199
Jacob McEwan S3841062 <s3841062@student.rmit.edu.au>
2022-09-02 15:21:35 +1000

    refactor: :recycle: Extract popout button to component

---
+++ src/lib/components/PopoutButton.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let popout = false;
+  export let side = "right";
+</script>
+
+<div class="popout-container">
+  <slot name="activator" open={() => popout = true} />
+  {#if popout}
+  <div class="popout {side}">
+    <slot />
+  </div>
+  {/if}
+</div>
+
+<style>
+  .popout-container {
+    position: relative;
+  }
+
+  .popout {
+    position: absolute;
+    top: 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    background: var(--color-bg-300);
+    border-radius: var(--border-radius);
+  }
+
+  .popout.right {
+    right: 0;
+  }
+
+  .popout.left {
+    left: 0;
+  }
+</style>
