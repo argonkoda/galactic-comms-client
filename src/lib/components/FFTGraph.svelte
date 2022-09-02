edde728d0e09408533441ac95cb11558d3bd6ab5
Jacob McEwan S3841062 <s3841062@student.rmit.edu.au>
2022-09-02 18:53:04 +1000

    feat: :lipstick: Change FFT graph style

--- src/lib/components/FFTGraph.svelte
+++ src/lib/components/FFTGraph.svelte
@@ -1,22 +1,34 @@
 <script>
+import { steam_id } from "../settings";
+
+
   export let fft;
 
   function updateFFTGraph(ctx, fft) {
     ctx.clearRect(0,0,ctx.canvas.width, ctx.canvas.height);
     ctx.strokeStyle = "#42ff9e";
+    ctx.fillStyle = "#42ff9e77";
     // ctx.strokeStyle = "var(--color-primary-200)";
     ctx.beginPath();
     ctx.moveTo(0, ctx.canvas.height/2);
     const fRange = (fft.length * 2 / 5) | 0;
+    const stepSize = ctx.canvas.width / (fRange + 2);
+    let [lastX, lastY] = [0, ctx.canvas.height/2];
+
     for (let i = 0; i < fRange; i ++) {
-      ctx.lineTo(ctx.canvas.width / (fRange + 2) * (i + 1), ctx.canvas.height / 2 - (fft[i]/256 * ctx.canvas.height / 2))
+      let [x, y] = [stepSize * (i + 1), ctx.canvas.height / 2 - (fft[i]/256 * ctx.canvas.height / 2)];
+      ctx.bezierCurveTo(lastX + stepSize/2 ,lastY, x - stepSize/2, y, x, y);
+      [lastX, lastY] = [x, y];
     }
     ctx.lineTo(ctx.canvas.width,ctx.canvas.height / 2);
+    [lastX, lastY] = [ctx.canvas.width,ctx.canvas.height / 2];
     for (let i = fRange-1; i >= 0; i --) {
-      ctx.lineTo(ctx.canvas.width / (fRange + 2) * (i + 1), ctx.canvas.height / 2 + (fft[i]/256 * ctx.canvas.height / 2))
+      let [x, y] = [stepSize * (i + 1), ctx.canvas.height / 2 + (fft[i]/256 * ctx.canvas.height / 2)];
+      ctx.bezierCurveTo(lastX - stepSize/2 ,lastY, x + stepSize/2, y, x, y);
+      [lastX, lastY] = [x, y];
     }
-    ctx.lineTo(0, ctx.canvas.height/2);
-
+    ctx.bezierCurveTo(lastX - stepSize/2, lastY, stepSize/2, ctx.canvas.height/2, 0, ctx.canvas.height/2);
+    ctx.fill();
     ctx.stroke();
   }
 
